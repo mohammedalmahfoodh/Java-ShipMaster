ed4b6fc8af05ae2935f36f0cc5ca7e07efb19db7
meamo <malm@kokumaion.com>
2020-06-23 08:38:24 +0200

    Add ignore file

--- db/initiateDB.bat
+++ db/initiateDB.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --protocol=tcp --host=localhost --user=root --password=tyfon --port=3306 --default-character-set=utf8 --comments   < "D:\courses\Java\My Advanced Projects\Kokumation Projects\Java-shipMaster\db\Create DataBaseShipMaster.sql"
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --protocol=tcp --host=localhost --user=root --password=tyfon --port=3306 --default-character-set=utf8 --comments --database=ship_master_java  < "D:\courses\Java\My Advanced Projects\Kokumation Projects\Java-shipMaster\db\Create Tables.sql"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --protocol=tcp --host=localhost --user=root --password=tyfon --port=3306 --default-character-set=utf8 --comments   < "F:\Java\Projects\Java-ShipMaster\db\Create DataBaseShipMaster.sql"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --protocol=tcp --host=localhost --user=root --password=tyfon --port=3306 --default-character-set=utf8 --comments --database=ship_master_java  < "F:\Java\Projects\Java-ShipMaster\db\Create Tables.sql"
 
 
 
